c2041dc8a245cd9ad1ad0a94346c04d4662172cb
Arbietd21 <145313814+Arbietd21@users.noreply.github.com>
2023-12-06 21:13:25 -0600

    updated passport.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const passport = require('passport');
 require('./passport')
 
 //return a list of movies  
-app.get('/movies', async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', {session: false}), async (req, res) => {
     await Movies.find()
     .then((movies) => {
         res.status(201).json(movies);
