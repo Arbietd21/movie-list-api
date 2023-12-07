1e4a023948156e8f7b6162635abf463515d1c021
Arbietd21 <145313814+Arbietd21@users.noreply.github.com>
2023-12-06 21:25:54 -0600

    updated auth.js

--- auth.js
+++ auth.js
@@ -22,14 +22,14 @@ module.exports = (router) => {
             if (error || !user) {
                 return res.status(400).json({
                     message: 'Something is not right',
-                    user: User
+                    user: user
                 });
             }
             req.login(user, {session: false}, (error) => {
                 if (error) {
                     res.send(error);
                 }
-                let token = generateJWTToken(usertoJSON());
+                let token = generateJWTToken(user.toJSON());
                 return res.json({user, token});
             });
         })(req, res);
