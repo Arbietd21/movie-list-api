a54b9e83f55f74076c6f75bf81bebd0e8870c213
Arbietd21 <145313814+Arbietd21@users.noreply.github.com>
2023-12-09 12:58:21 -0600

    updated passport.js

--- passport.js
+++ passport.js
@@ -23,11 +23,9 @@ passport.use(
                         message: 'Incorrect username or password.',
                     });
                 }
-                if (!user) {
-                    console.log('incorrect username');
-                    return callback(null, false, {
-                        message: 'Incorrect username or password.'
-                    });
+                if (!user.validatePassword(password)) {
+                    console.log('incorrect password');
+                    return callback(null, false, {message: 'Incorrect Password'});
                 }
                 console.log('finished');
                 return callback(null, user);
