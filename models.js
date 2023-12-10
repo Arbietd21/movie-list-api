bb417c87050765e7f247b5b3923cf05572efb0ab
Arbietd21 <145313814+Arbietd21@users.noreply.github.com>
2023-12-10 13:26:06 -0600

    updated model.js

--- models.js
+++ models.js
@@ -17,19 +17,19 @@ let movieSchema = mongoose.Schema({
 });
 
 let userSchema = mongoose.Schema({
-    Username: {type: String, required: true},
-    Password: {type: String, required: true},
-    Email: {type: String, required: true},
-    Birthday: Date,
-    Favorites: [{type: mongoose.Schema.Types.ObjectID, ref: 'movie'}]
+Username: {type: String, required: true},
+Password: {type: String, required: true},
+Email: {type: String, required: true},
+Birthday: Date,
+Favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
 userSchema.statics.hashPassword = (password) => {
-    return bcrypt.hashSync(password, 10);
+return bcrypt.hashSync(password, 10);
 };
 
 userSchema.methods.validatePassword = function(password) {
-    return bcrypt.compareSync(password, this.Password);
+return bcrypt.compareSync(password, this.Password);
 };
 
 let Movie = mongoose.model('Movie', movieSchema);
