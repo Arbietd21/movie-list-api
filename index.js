e04fbb9f0f3154821e0d7927284cb3b5fde9dcef
Arbietd21 <145313814+Arbietd21@users.noreply.github.com>
2024-01-08 20:38:22 -0600

    updated index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ require('./passport')
 const { check, validationResult } = require('express-validator');
 
 //return a list of movies  
-app.get('/movies', async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
