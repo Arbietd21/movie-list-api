92fde0608fcacc770c84ecd4efd7d5d233456fb7
Arbietd21 <145313814+Arbietd21@users.noreply.github.com>
2024-01-14 11:03:24 -0600

    updated auth.js

--- auth.js
+++ auth.js
@@ -2,14 +2,14 @@ const jwtSecret = 'your_jwt_secret';
 //this has to be the same key used in the JWTStrategy
 
 const jwt = require('jsonwebtoken'),
-  passport = require('passport');
+    passport = require('passport');
 
 require('./passport');
 //your local passport file. does this need to have '.js' after it?
 
 let generateJWTToken = (user) => {
     return jwt.sign(user, jwtSecret, {
-        subject: user.Username, //this is the username you're encoding in the JWT
+        subject: user.username, //this is the username you're encoding in the JWT
         expiresIn: '7d', //This specifies that the token will expire in 7 days
         algorithm: 'HS256' //this is the algorithm used to "sign" or encode the values of the JWT
     });
@@ -18,19 +18,19 @@ let generateJWTToken = (user) => {
 /* POST login */
 module.exports = (router) => {
     router.post('/login', (req, res) => {
-        passport.authenticate('local', {session: false}, (error, user, info) => {
+        passport.authenticate('local', { session: false }, (error, user, info) => {
             if (error || !user) {
                 return res.status(400).json({
                     message: 'Something is not right',
                     user: user
                 });
             }
-            req.login(user, {session: false}, (error) => {
+            req.login(user, { session: false }, (error) => {
                 if (error) {
                     res.send(error);
                 }
                 let token = generateJWTToken(user.toJSON());
-                return res.json({user, token});
+                return res.json({ user, token });
             });
         })(req, res);
     });
