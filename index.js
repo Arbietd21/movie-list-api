ec76d906e349ef503afca14c6b8e3455b8a7040c
Arbietd21 <145313814+Arbietd21@users.noreply.github.com>
2024-01-22 18:27:47 -0600

    updated index.js

--- index.js
+++ index.js
@@ -234,7 +234,7 @@ app.delete('/users/:username/favorites/:MovieID', passport.authenticate('jwt', {
         return res.status(400).send('Permission Denied');
     }
 
-    await Users.findOneAndDelete({ username: req.params.username }, {
+    await Users.findOneAndUpdate({ username: req.params.username }, {
         $delete: { favorites: req.params.MovieID }
     },
         { new: true })
